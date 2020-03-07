bfa42816438347b602314b151bd969e9a280d5d7
Jelly38214 <goolezheng@gmail.com>
2020-03-07 23:52:40 +0800

    feature: Add useInterval hook.

---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,22 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export function useInterval(fn: Function, time: number) {
+  const callback = useRef(fn);
+  const timer = useRef(-1);
+  const canceler = useCallback(() => {
+    if (timer.current !== -1) {
+      clearInterval(timer.current);
+    }
+  }, []);
+
+  callback.current = fn; // for updating when fn change
+
+  useEffect(() => {
+    const tick = setInterval(fn, time);
+    timer.current = tick;
+
+    return () => clearInterval(tick);
+  }, [time]);
+
+  return canceler;
+}
